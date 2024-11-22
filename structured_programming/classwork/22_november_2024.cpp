39cba3f667765bdc673f34a3f6d908e9040d0b50
Max-Kali <maxim.meson@gmail.com>
2024-11-22 10:42:27 +0200

    feat(structuredPrograming): work done in theory class

---
+++ structured_programming/classwork/22_november_2024.cpp
@@ -0,0 +1,29 @@
+// Strucutra de date - colectie de date organizata dupa anumite reguli, care depind de tipul de structura
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    // int a[5], n;
+
+    // cout << "Introdu nr. elementelor: "; cin >> n;
+    // for (int i = 0; i < n; i++) {
+    //     cout << "a[" << i << "] = "; cin >> a[i];
+    // }
+
+    // cout << "\n----Rezultatele----" << endl;
+    // for (int i = 0; i < n; i++) {
+    //     cout << "a[" << i << "] = " << a[i] << endl;
+    // }
+
+    // int a[5][3] = {
+    //     {1, 2, 3},
+    //     {4, 5, 6},
+    //     {7, 8, 9},
+    //     {10, 11, 12},
+    //     {13, 14, 15}
+    // };
+
+    // cout << a[1][11] << endl;
+    return 0;
+}
