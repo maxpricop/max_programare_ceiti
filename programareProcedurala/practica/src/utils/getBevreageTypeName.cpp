a6dd99d301b797018a5757cd0ec627b199f8643f
Max-Kali <maxim.meson@gmail.com>
2025-05-13 13:13:38 +0300

    feat(programareProcedurala/practica): created a util function to get beverage type names

---
+++ programareProcedurala/practica/src/utils/getBevreageTypeName.cpp
@@ -0,0 +1,23 @@
+#include "getBevreageTypeName.hpp"
+#include "../types/Beverages.hpp"
+#include "string"
+
+std::string getBeverageTypeName(BeverageType type) {
+    switch (type) {
+        case Sweet: {
+            return "Apa dulce";
+        }
+        case Mineral: {
+            return "Apa minerala";
+        }
+        case Juice: {
+            return "Suc";
+        }
+        case Syrup: {
+            return "Sirop";
+        }
+        default: {
+            return "Unknown";
+        }
+    }
+}
---
+++ programareProcedurala/practica/src/utils/getBevreageTypeName.hpp
@@ -0,0 +1,9 @@
+#ifndef GetBevreageTypeNameHpp
+#define GetBevreageTypeNameHpp
+
+#include "../types/Beverages.hpp"
+#include "string"
+
+std::string getBeverageTypeName(BeverageType type);
+
+#endif
