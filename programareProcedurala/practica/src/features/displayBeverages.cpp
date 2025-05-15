11feedf38e4badef2dc378897d6f448112ada92d
Max-Kali <maxim.meson@gmail.com>
2025-05-15 13:46:40 +0300

    refactor(programareProcedurala/practica/features): improved the table displaying beverages and deliveries

--- programareProcedurala/practica/src/features/displayBeverages.cpp
+++ programareProcedurala/practica/src/features/displayBeverages.cpp
@@ -1,7 +1,9 @@
 #include "displayBeverages.hpp"
 #include "../types/Beverages.hpp"
-#include "../utils/getBevreageTypeName.cpp"
+#include "../utils/createRepeatedString.hpp"
+#include "../utils/getBevreageTypeName.hpp"
 #include "fstream"
+#include "iomanip"
 #include "iostream"
 #include "string"
 #include "vector"
@@ -18,7 +20,7 @@ void displayBeverages(const std::string &filename) {
     Beverage currentBeverage;
 
     // Expected line layout:
-    // ID (unsigned), Type (BeverageType), name (string), color (string), pricePerLiter(double)
+    // ID (unsigned), Type (BeverageType), name (string), color (string), pricePerLiter (float)
     while (file >> currentBeverage.id) {
         int rawBeverageType;
         file >> rawBeverageType >> currentBeverage.name >> currentBeverage.color >> currentBeverage.pricePerLiter;
@@ -27,8 +29,37 @@ void displayBeverages(const std::string &filename) {
         beverageList.push_back(currentBeverage);
     }
 
-    for (const Beverage &bev : beverageList) {
-        std::cout << bev.id << " " << getBeverageTypeName(bev.type) << " " << bev.name << " " << bev.color << " "
-                  << bev.pricePerLiter << "\n";
+    // Column widths
+    const int wId = 6;
+    const int wName = 25;
+    const int wType = 17;
+    const int wColor = 17;
+    const int wPrice = 6;
+
+    // Top of the table
+    std::cout << createRepeatedString("╔", "═", wId, "╦") << createRepeatedString("═", "═", wName - 1, "╦")
+              << createRepeatedString("═", "═", wType - 1, "╦") << createRepeatedString("═", "═", wColor - 1, "╦")
+              << createRepeatedString("═", "═", wPrice - 1, "╗\n");
+
+    // Line displaying all table columns
+    std::cout << std::left << "║" << std::setw(wId) << "ID" << "║" << std::setw(wName) << "Nume" << "║"
+              << std::setw(wType) << "Tip" << "║" << std::setw(wColor) << "Culoare" << "║" << std::setw(wPrice)
+              << "Pret/L" << "║\n";
+
+    // Divider between table header and data
+    std::cout << createRepeatedString("╠", "═", wId, "╬") << createRepeatedString("═", "═", wName - 1, "╬")
+              << createRepeatedString("═", "═", wType - 1, "╬") << createRepeatedString("═", "═", wColor - 1, "╬")
+              << createRepeatedString("═", "═", wPrice - 1, "╣\n");
+
+    // Display table data, information about all beverages
+    for (const auto &bev : beverageList) {
+        std::cout << std::left << "║" << std::setw(wId) << bev.id << "║" << std::setw(wName) << bev.name << "║"
+                  << std::setw(wType) << getBeverageTypeName(bev.type) << "║" << std::setw(wColor) << bev.color << "║"
+                  << std::setw(wPrice) << bev.pricePerLiter << "║\n";
     }
+
+    // End of table
+    std::cout << createRepeatedString("╚", "═", wId, "╩") << createRepeatedString("═", "═", wName - 1, "╩")
+              << createRepeatedString("═", "═", wType - 1, "╩") << createRepeatedString("═", "═", wColor - 1, "╩")
+              << createRepeatedString("═", "═", wPrice - 1, "╝\n");
 };
--- programareProcedurala/practica/src/features/displayDeliveries.cpp
+++ programareProcedurala/practica/src/features/displayDeliveries.cpp
@@ -1,6 +1,8 @@
 #include "displayDeliveries.hpp"
 #include "../types/Delivery.hpp"
+#include "../utils/createRepeatedString.hpp"
 #include "fstream"
+#include "iomanip"
 #include "iostream"
 #include "string"
 #include "vector"
@@ -23,7 +25,30 @@ void displayDeliveries(const std::string &filename) {
         deliveriesList.push_back(currentDelivery);
     }
 
-    for (const Delivery &delivery : deliveriesList) {
-        std::cout << delivery.id << "\t" << delivery.beverageId << "\t" << delivery.quantityDelivered << "\n";
+    // Column widths
+    const unsigned wId = 6;
+    const unsigned wBeverageId = 12;
+    const unsigned wQuantity = 18;
+
+    // Top of the table
+    std::cout << createRepeatedString("╔", "═", wId, "╦") << createRepeatedString("═", "═", wBeverageId - 1, "╦")
+              << createRepeatedString("═", "═", wQuantity - 1, "╗\n");
+
+    // Line displaying all table columns
+    std::cout << std::left << "║" << std::setw(wId) << "ID" << "║" << std::setw(wBeverageId) << "ID Bautura"
+              << "║" << std::setw(wQuantity) << "Cantitate livrata" << "║\n";
+
+    // Divider between table header and data
+    std::cout << createRepeatedString("╠", "═", wId, "╬") << createRepeatedString("═", "═", wBeverageId - 1, "╬")
+              << createRepeatedString("═", "═", wQuantity - 1, "╣\n");
+
+    // Display table data, information about all deliveries
+    for (const auto &delivery : deliveriesList) {
+        std::cout << std::left << "║" << std::setw(wId) << delivery.id << "║" << std::setw(wBeverageId)
+                  << delivery.beverageId << "║" << std::setw(wQuantity) << delivery.quantityDelivered << "║\n";
     }
+
+    // End of table
+    std::cout << createRepeatedString("╚", "═", wId, "╩") << createRepeatedString("═", "═", wBeverageId - 1, "╩")
+              << createRepeatedString("═", "═", wQuantity - 1, "╝\n");
 };
