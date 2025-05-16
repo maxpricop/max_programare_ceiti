eaf1f5a7515270432c3165262339076631a0bcd2
Max-Kali <maxim.meson@gmail.com>
2025-05-16 13:23:20 +0300

    refactor(programareProcedurala/practica/features/beverages): added better variable name for the new beverage

--- programareProcedurala/practica/src/features/beverages/addNewBeverage.cpp
+++ programareProcedurala/practica/src/features/beverages/addNewBeverage.cpp
@@ -7,26 +7,26 @@
 #include "utils/appendLine.hpp"
 
 void addNewBeverage(const std::string &filename) {
-    Beverage bev{};
+    Beverage newBeverage{};
     int rawBeverageType;
 
     std::cout << "Introduceti ID bautura: ";
-    std::cin >> bev.id;
+    std::cin >> newBeverage.id;
 
     std::cout << "Introduceti tip bautura (0=Apa dulce,1=Minerala,2=Suc,3=Sirop): ";
     std::cin >> rawBeverageType;
-    bev.type = static_cast<BeverageType>(rawBeverageType);
+    newBeverage.type = static_cast<BeverageType>(rawBeverageType);
 
     std::cout << "Introduceti numele bauturii: ";
-    std::cin >> bev.name;
+    std::cin >> newBeverage.name;
 
     std::cout << "Introduceti culoarea bauturii: ";
-    std::cin >> bev.color;
+    std::cin >> newBeverage.color;
 
     std::cout << "Introduceti pretul pe litru: ";
-    std::cin >> bev.pricePerLiter;
+    std::cin >> newBeverage.pricePerLiter;
 
-    appendLine(
-        {std::to_string(bev.id), std::to_string(bev.type), bev.name, bev.color, std::to_string(bev.pricePerLiter)},
-        filename);
+    appendLine({std::to_string(newBeverage.id), std::to_string(newBeverage.type), newBeverage.name, newBeverage.color,
+                std::to_string(newBeverage.pricePerLiter)},
+               filename);
 };
