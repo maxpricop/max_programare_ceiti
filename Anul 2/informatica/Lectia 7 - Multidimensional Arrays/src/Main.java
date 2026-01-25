c8f95ffa047f8f4202bd9dd2769264e7f2ac083c
Max-Kali <maxim.meson@gmail.com>
2026-01-25 23:52:47 +0200

    feat(informatica): lucru practic cu multidimensional arrays

---
+++ Anul 2/informatica/Lectia 7 - Multidimensional Arrays/src/Main.java
@@ -0,0 +1,10 @@
+import model.CheltuieliAnuale;
+
+public class Main {
+    public static void main(String[] args) {
+        CheltuieliAnuale cheltuieli = new CheltuieliAnuale();
+
+        System.out.println(cheltuieli.calculateYearlyCheltuieli());
+    }
+
+}
